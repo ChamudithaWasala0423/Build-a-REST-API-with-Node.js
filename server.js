d82b52a3b621334d56f8be1f009e5949e662719b
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-12-25 13:37:15 +0530

    change content

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Success");
 });
 
+const blog = [];
+
 app.get("/blogs", (req, res) => {
-  res.send("Blogs called!");
+  res.send(blog);
 });
+
+app.post("/blogs", (req, res) => {
+  blog.push({ id: blog.length + 1, ...req.body });
+  res.status(201).send("Success");
+});
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
