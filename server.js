a12a2d5d1fe6666d57258acb60a5201b708c78bd
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-12-25 13:12:38 +0530

    Get blog response

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
 app.get("/", (req, res) => {
   res.send("Success");
 });
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.get("/blogs", (req, res) => {
+  res.send("Blogs called!");
+});
