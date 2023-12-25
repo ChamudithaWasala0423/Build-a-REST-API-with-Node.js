40993a36fafc94001c13f2f5b8d9da06f84e9eb5
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-12-25 15:51:00 +0530

    put and  get access single object

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//create a middleware for converting json to object
 app.use(express.json());
+
+//create a middleware for convert id
+const convertId = (req, res, next) => {
+  const id = req.params.id;
+  req.params.id = parseInt(id);
+  next();
+};
 
 app.get("/", (req, res) => {
   res.send("Success");
@@ -16,7 +24,22 @@ app.get("/blogs", (req, res) => {
 
 app.post("/blogs", (req, res) => {
   blog.push({ id: blog.length + 1, ...req.body });
-  res.status(201).send("Success");
+  res.status(201).send("Success post");
+});
+
+app.put("/blogs/:id", convertId, (req, res) => {
+  const id = req.params.id;
+  const index = blog.findIndex((item) => item.id === id);
+  //console.log(index);
+  blog[index] = req.body;
+  res.send(blog[index]);
+});
+
+//REST API can asccess single object
+app.get("/blogs/:id", convertId, (req, res) => {
+  const id = req.params.id;
+  const index = blog.findIndex((item) => item.id === id);
+  res.send(blog[index]);
 });
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
